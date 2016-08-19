b881195c016c7030e071a34cd95cd40a54563720
pwjablonski Jablonski <pwjablonski@gmail.com>
2016-08-19 20:41:45 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
+function getIt(){
+  $('p').on("click", function(){
+  //action you want taken
+    alert("hi!")
+  })
+}
+
+function frameIt(){
+  $('img').on("load", function(){
+  //action you want taken
+    $('img').addClass('tasty')
+  })
+}
+
+function submitIt(){
+  $('form').on("submit", function(){
+  //action you want taken
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+function pressIt(){
+  $('#typing').on("keydown", function(key){
+  //action you want taken
+    if (key.which == 71){
+      alert('g was pressed');
+    }
+  })
+}
+
 
 $(document).ready(function(){
 
 // call functions here
+getIt()
 
 });
