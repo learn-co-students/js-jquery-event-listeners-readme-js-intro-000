523cc40a9c76530f3bf209fb8b504f2ff104c535
mo6709 <mo6709@gmail.com>
2016-08-20 23:13:40 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,39 @@
 $(document).ready(function(){
 
 // call functions here
+getIt();
 
 });
+
+function getIt() {
+	// body...
+	$('p').on('click', function () {
+		// body...
+		alert("Hey!");
+	});
+};
+
+function frameIt(argument) {
+	// body...
+	$('img').on('load', function() {
+        $('img').addClass('tasty');
+	});
+};
+
+function pressIt() {
+	// body...
+	$('input').on('keydown', function (e){
+        var key = parseInt(e.which || e.detail);
+        if (key == 71){
+        	alert("WOW, you prassed g");
+        };
+	});
+};
+
+function submitIt() {
+	// body...
+    $('form').on('submit', function (){
+        alert("Your form is going to be submitted now.");
+    });
+};
+
