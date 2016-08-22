4fc578e470ffe2444d568eb76e85b4a11ffe44e3
Peter Tascio <peter.tascio@gmail.com>
2016-08-22 16:18:51 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,42 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!");
+    return;
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+     $('img').addClass("tasty");
+  });
+}
+
+function submitIt(){
+    $("form").on("submit", function(){
+      //var value = $("input:first").val();
+      // if(value.length > 0){
+      //   alert("Your form is going to be submitted now.");
+      //   return;
+      // }
+      // alert("You haven't entered anything yet.");
+      // return;
+      alert("Your form is going to be submitted now.");
+    });
+}
+
+function pressIt(){
+  $(document).on('keydown', function(key){
+    if(key.which === 71){
+      alert("You pressed the 'g' key!");
+    }
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  // call functions here
+  getIt();
+  frameIt();
+  submitIt();
+  pressIt();
 });
