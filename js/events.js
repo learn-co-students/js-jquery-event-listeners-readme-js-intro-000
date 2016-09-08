c6e9556cbfcd0b52e23e7149a7b40d243cb9d5e3
Stephen Higgins <stephenhiggins76@gmail.com>
2016-09-08 15:04:53 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
 
 $(document).ready(function(){
+getIt()
+frameIt()
+pressIt()
 
 // call functions here
 
 });
+
+function getIt() {
+  $('p').on('click', function(){
+    return window.alert('Hey!')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('form input:first-child').on('keydown', function(key) {
+    if (key.which === 71) {
+    return window.alert('You have pressed g!')
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    return window.alert('Your form is going to be submitted now.')
+  })
+}
