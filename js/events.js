87818b7c4d12ee44ca293b1260695e0372c596a7
Danny Marquez <dannybrianmarquez@gmail.com>
2016-09-16 19:04:22 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
+var getIt = function() {
+  $('p').on("click", function() {
+    alert("Hey!");
+  });
+}
+
+var frameIt = function() {
+  var tasty = ".tasty { border : '1px solid red'; }"
+  $('img').on("load", function() {
+    $('img').addClass("tasty");
+  });
+}
+
+var pressIt = function() {
+  $('form input').on("keydown", function(key) {
+    if (key.which === 71) {
+      alert("You have pressed g");
+    }
+  });
+}
+
+var submitIt = function() {
+  $('form').on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  });
+}
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
