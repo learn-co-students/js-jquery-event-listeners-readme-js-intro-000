1958d9ce412fb33562e2fe0b3e39250590113c87
Kyle Kampa <kyle.kampa@gmail.com>
2016-09-20 00:42:01 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
-//define functions here
+function getIt(){
+  $('p').on('click',function(){
+    alert('Hey!')
+  }
+)}
 
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').attr("class","tasty")
+  })
+}
+
+function pressIt(){
+  $('#typing').on('keydown', function(key){
+    if(key.which == 71){
+      alert('you pushed my G spot baby ;)')
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.')
+  })
+}
 $(document).ready(function(){
 
-// call functions here
+getIt()
+frameIt()
+pressIt()
+submitIt()
 
 });
