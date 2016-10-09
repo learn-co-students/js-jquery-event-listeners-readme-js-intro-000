d5b1ceb7a8a89fb6713c148f1979697d4a45f916
Rakesh Grewal <grewalrakesh@yahoo.co.in>
2016-10-09 11:11:53 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,26 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+function getIt(){
+
+  $('p').on('click',function(event){ alert('Hey!')})
+}
+
+function frameIt(){
+ $('img').on('load', function (){
+   $('img').addClass('tasty')
+ })
+}
+
+function pressIt(){
+  $('input').on('keydown', function(key) {
+  if (key.which === 71){
+    alert('g was pressed');
+  }
+});
+}
+
+function submitIt(){
+  $(document).on('submit',function(){ alert("Your form is going to be submitted now.")})
+}
