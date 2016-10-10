b8df4db96c8776798faaeee288d2137660210d0f
Villy Siu <villysiu@gmail.com>
2016-10-10 23:06:29 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,27 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt(){
+  $('p').on("click", function(){
+    alert("Hey!");
+  })
+}
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').attr('class', 'tasty')
+  });
 
-// call functions here
+}
+function pressIt(){
+  $("form").on('keydown', function(key) {
+    if(key.which == 71){
+      alert('g was pressed');
+    }
+  });
+}
 
-});
+function submitIt(){
+  $("form").on("submit", function() {
+      alert('Your form is going to be submitted now.');
+      return;
+  });
+}
