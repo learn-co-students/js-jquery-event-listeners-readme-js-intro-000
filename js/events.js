636f944bb3362ddfaf8380c9ced14b12b5aa7481
kiran bhat <k_bhat@yahoo.com>
2016-10-15 01:42:39 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
-
 $(document).ready(function(){
-
-// call functions here
+ console.log("Document ReadY");
+$('p').on('click',getIt);
+$('img').on('click', frameIt);
+$(document).on('submit', submitIt);
+$('#typing').on('keydown', {'test':'7'}, pressIt);
 
 });
+
+function getIt(e) {
+  alert('GET IT');
+}
+
+function frameIt () {
+  $('img').addClass("tasty");
+}
+
+function pressIt () {
+//  alert("g key has been pressed");
+$('#typing').on('keydown', function(event){
+
+  if ( event.which=== 71) {
+  alert("g key has been pressed");}
+ })
+
+
+
+}
+
+function submitIt () {
+alert ("Your form is going to be submitted now.");
+
+}
