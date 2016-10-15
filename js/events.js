bbd40ff004602cd5f83b843b2b14b911482231ee
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2016-10-14 23:45:31 -0400

    write functions that facilitate interaction and call them in window.ready

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
 //define functions here
+function getIt(){
+	$('p').on("click", function(){
+		alert("Hey!");
+
+});}
+
+function frameIt(){
+	$('img').on('load', function(){
+  	$('img').addClass("tasty");
+});
+}
+
+function pressIt(){
+	$('input').on('keydown', function(key){
+		if(key.which == 71){
+			alert("Yep, that's a g, G.")
+		}
+	})
+}
+
+function submitIt(){
+	$('form').on('submit', function(){
+		alert("yer form gonna submit now")
+	})
+}
 
 $(document).ready(function(){
-
+	getIt();
+	frameIt();
+	pressIt();
+	submitIt();
 // call functions here
 
 });
