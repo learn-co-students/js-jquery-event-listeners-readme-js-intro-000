f5564e41c87ceb17e6a8f8c0280237cab5e8f30e
Timothy Flores <tim@timflores.com>
2016-10-25 19:05:10 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,40 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function() {
+    alert('hey!')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+
+    if(key.which === 71) {
+      alert("You hit the g!")
+    }
+
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
 
 });
+
+
+// $('h1').on("eventname", function(){
+//     //action you want taken
+// });
