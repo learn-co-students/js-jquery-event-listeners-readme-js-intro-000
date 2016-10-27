15d4ab67d6eb325283f4b420afafb32f62029189
Matt Fender <mjfender@gmail.com>
2016-10-27 00:09:56 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+  })
+}
+
+// bind load event to add "tasty" class to the image (adding red outline)
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  })
+}
+
+// bind keydown event to input field and alerts user when they press 'g' key
+function pressIt(key) {
+  $('#typing').on('keydown', function(key) {
+  if(key.which == 71){
+      alert('g was pressed');
+  }
+})
+}
+
+// binds a submit event to form and alerts user when form will be submitted
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.");
+    return;
+  })
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
