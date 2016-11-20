e989ba24be2945a18066f7cc34092e72053118e4
John Simoni <jsimoni03@gmail.com>
2016-11-20 04:48:05 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,34 @@
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+submitIt();
 
 });
+
+var getIt = function(){
+  $('p').on("click", function(){
+    alert("Hey!");
+  });
+};
+
+var frameIt = function(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  });
+};
+
+var submitIt = ()=>{
+  $('form').on('submit', ()=>{
+    alert("Your form is going to be submitted now.");
+  });
+};
+
+var pressIt = ()=>{
+  $(document).on('keydown', function(key){
+    if (key.which === 71){
+      alert("You pressed g!");
+    }
+  });
+};
