65611bb3c43fb462777bc60f89c122779d7f07cb
gina hicks <ginahixx@gmail.com>
2016-11-27 08:01:42 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,37 @@
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
+
+function getIt() {
+  $('p').on("click", function(){
+    alert("Hey!")
+});
+}
+
+function frameIt() {
+  $('img').on("load", function(){
+    console.warn("in frame it before tasty assignment");
+    $('img').addClass('tasty')
+});
+}
+
+function pressIt() {
+  $('#typing').on("keydown", function(event){
+    console.warn("in keydown key is " + (event.detail || event.which));
+    //if(event.key==="g"){
+    if((event.detail || event.which)===71){
+      alert("You have pressed g")
+    }
+});
+}
+
+function submitIt() {
+  $('form').on("submit", function(){
+      alert("Your form is going to be submitted now.")
+});
+}
