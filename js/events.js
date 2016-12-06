347ac5e9a52b7c6be7166311e3f97c489e88d6b4
Travis Payne <thpaggie910@gmail.com>
2016-12-06 16:52:28 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,34 @@
 
 $(document).ready(function(){
 
-// call functions here
+  getIt();
+  frameIt();
+  submitIt();
+  pressIt();
 
 });
+
+function getIt() {
+  $('p').on('click', () => {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('img').on('load', () => {
+    $('img').attr('class', 'tasty');
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', () => {
+    alert('Your form is going to be submitted now.');
+  });
+}
+
+function pressIt() {
+  $('#typing').on('keydown', (key) => {
+    if(key.which === 71)
+      alert('you pressed the right key!');
+  });
+}
