b6642080bc3b420bc7e76b8b3f90662d89179a5c
weimin dong <dwmdwm2001910@gmail.com>
2016-12-10 04:08:01 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,28 @@ $(document).ready(function(){
 // call functions here
 
 });
+function getIt() {
+$('p').on('click', function() {
+  alert('Hey!');
+})
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').attr('class', 'tasty');
+  })
+}
+
+function pressIt() {
+  $('body').on('keydown', function(key) {
+    if(key.which == 71) {
+      alert('the g key was pressed down!');
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+  })
+}
