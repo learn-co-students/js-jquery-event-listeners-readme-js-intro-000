59650ef2ac8074206a84a28fcbac4fdd6cac7a3a
David Heisner <davidheis@hotmail.com>
2016-12-23 15:22:57 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
-//define functions here
+function getIt(){
+  $('p').on('click', alert('Hey!'))
+}
+
+function frameIt(){
+  $('img').addClass('tasty')
+}
+
+function pressIt(){
+   $('#typing').on('keydown', function(e){
+    if(e.which === 103){
+      alert('You pressed g')
+    }
+  })
+
+}
+
+function submitIt(){
+  $('type[submit]').on('submit', function(){
+    alert('"Your form is going to be submitted now."')
+  })
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+ getIt();
+ frameIt();
+ pressIt();
+ submitIt();
 });
