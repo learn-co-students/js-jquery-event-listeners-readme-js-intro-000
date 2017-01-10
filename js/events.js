243ebee66b3b93ca9ada53fb0ceeca3a56efa305
Ines Pucker <ines.pucker@gmx.de>
2017-01-10 11:02:15 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,6 +1,26 @@
-//define functions here
+function getIt () {$('p').on('click', function(){
+ alert("Hey!");
+  return;
+});
+return;
+                  }
+
+function  frameIt () {$('*').on('load', function(){
+ $('img').append(' class="tasty"');
+  return;
+});
+
+
+
+
+
+
+
+
 
 $(document).ready(function(){
+getIt ();
+frameIt ();
 
 // call functions here
 
