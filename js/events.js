dba38b11f57a5a28e2bdf4d1726d1f1aa79743d6
Britt Cumens <britt.cumens@gmail.com>
2017-01-17 13:45:30 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt() {
+    alert('Hey!')
+}
+
+function frameIt() {
+    $('img').attr( 'class', 'tasty' )
+}
+
+function pressIt(){
+    $('#typing').on('keydown', function(key){
+      if(key.which == 71){
+          alert('G whiz.');
+      }
+    })
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.')
+  })
+}
 
 $(document).ready(function(){
+//call functions here
 
-// call functions here
+ $('p').on('click', getIt)
+ $('img').on('load', frameIt)
+ $('form').on('keydown', pressIt)
+ //when pressIt is called, the alert shows up in the window more than one time. Not sure how to fix this.
+ $('form').on('submit', submitIt)
 
 });
