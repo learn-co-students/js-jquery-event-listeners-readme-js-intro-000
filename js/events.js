e0f38268d89de22b98d6cdac551cdcd4b1e444df
Muhammad Mehdi <mohummad.mehdi@gmail.com>
2017-01-22 16:24:58 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,26 @@
 //define functions here
+function getIt(){
+$('p').on("click", function(){
+  alert("Hey!")
+})
+};
 
-$(document).ready(function(){
+function frameIt(){
+$('img').on("load", function (){
+  $('img').addClass('tasty')
+})
+};
 
-// call functions here
-
-});
+function submitIt(){
+  $('form').on ("submit", function (){
+    alert("Your form is going to be submitted now.")
+    return;
+  })
+};
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if(key.which == 71){
+      alert('g was pressed');
+    }
+  })
+}
