d0f35087b5c931dfae2dc95dbec9dcdb376cae6a
Danilo Sanchez <danilosanchez870@gmail.com>
2017-02-18 08:59:22 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
-//define functions here
+function getIt(){
+  $(`p`).on("click", function(e){
+  alert("Hey!")
+})
+}
+
+function frameIt(){
+  $(`img`).on("load", function(e){
+  $(this).addClass('tasty');
+})
+}
+
+function submitIt(){
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  })
+}
+function pressIt(){
+  $(`form`).on('keydown', function(key) {
+  if (key.which == 71){
+    alert('enter was pressed');
+  }
+});
+}
+
 
 $(document).ready(function(){
-
-// call functions here
+getIt();
+frameIt();
+submitIt();
+pressIt()
 
 });
