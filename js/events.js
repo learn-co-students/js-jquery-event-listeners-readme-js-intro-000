5d0089732ea90f49dffe65138974d984716e2975
Mike McNeeley <mmcneeley@gmail.com>
2017-02-19 23:34:14 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt () {
+  $('p').on('click', function(){
+    alert('Hey!')
+  })
+}
 
+function frameIt () {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+    $('img').css('border: 1px solid red')
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if (key.which === 71) {
+      alert('g was pressed');
+    }
+  })
+}
+
+function submitIt () {
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.');
+  })
+}
+
+
+$(document).ready(function(){
 // call functions here
+
+getit();
+frameIt();
+pressIt();
+submitIt();
 
 });
