d11bf8c1263b55c4cb0e2c109f6b162317104c8a
R G <rafaelgerena@gmail.com>
2017-04-16 20:34:32 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,26 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+  })
+}
 
-$(document).ready(function(){
 
-// call functions here
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
 
-});
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if (key.which === 71) {
+      alert("You pressed 'g'!")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  })
+}
