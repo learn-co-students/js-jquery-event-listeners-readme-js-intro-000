8d6ab5d1031a2af0d83c7e7507b278652774cb73
Casey Tyack <kazenorite@gmail.com>
2017-04-21 00:18:13 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
 
 $(document).ready(function(){
-
+ getIt()
+ frameIt()
+ pressIt()
+ submitIt()
 // call functions here
 
 });
+function getIt() {
+  $("p").on('click', function(click) {
+    alert("Hey!");
+    return;
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $("img").addClass("tasty");
+    return;
+  });
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if (key.which == 71) {
+      alert("You have pressed the g key.")
+    }
+    return;
+  })
+}
+
+function submitIt() {
+  $(document).on("submit", function() {
+    alert("Your form is going to be submitted now.")
+    return;
+  })
+}
