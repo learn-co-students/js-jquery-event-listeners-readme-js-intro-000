39777c3927fa31b72c67aa24ec09a45fb3021c53
Valerie Corvington Farrell <vcorvington@gmail.com>
2017-05-12 03:20:39 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,40 @@
 //define functions here
 
 $(document).ready(function(){
-
-// call functions here
-
 });
+
+function getIt() {
+$( "p" ).click(function() {
+  alert( "Hey!" );
+});
+}
+
+
+function frameIt() {
+  $('img').load(function() {
+    $("img").addClass("tasty");
+     })
+}
+
+
+function pressIt() {
+  $('form').keydown(function(key) {
+    if (key.which === 71) {
+      alert( "Hey G!" );
+         }
+     })
+}
+/*function submitIt() {
+  $('form').keydown(function(key) {
+    if (key.which === 71) {
+      alert( "Hey G!" );
+         }
+     })
+}
+*/
+
+function submitIt(){
+   $("form").on("submit", function(){
+     alert("Your form is going to be submitted now.")
+   })
+ }
