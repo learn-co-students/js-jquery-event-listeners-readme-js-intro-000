bd6ff50114a3847d6292e2e7e2f699872f9871c7
Joseph White <josephcwhite@outlook.com>
2017-06-04 19:23:49 -0700

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,56 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+
+function getIt() {
+    $('p').on('click', function() { 
+        alert("Hey!");
+    });
+}
+
+function frameIt() {
+    $('img').on('load', function(){
+        $('img').addClass('tasty');
+    }); 
+}
+
+function pressIt() {
+    $('#typing').on('keydown', function(key) {
+        console.log(key.which)
+        if(key.which === 71) {
+            alert("You pressed the G key!")   
+        }
+    });
+}
+
+
+function submitIt () {
+    $('form').on("submit", function() {
+        if( $('input:first').val() ) {
+          alert("Your form is going to be submitted now."); 
+        } else {
+          alert("Your form is going to be submitted now.");   
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
