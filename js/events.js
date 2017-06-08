f5e29ffa220431d20dc3da36e70a40fb860d2b40
Jennifer Volpe <jvolpe721@gmail.com>
2017-06-08 16:58:22 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,17 +1,42 @@
 //define functions here
 function getIt() {
-    $ {"p"}.on("click", function() {
+    $("p").on("click", function() {
         alert('Hey!');
         return;
     });
 }
-function frameIt() {
-   $('img').on('load', function() {
-     $('img').addClass('tasty');
+
+ function frameIt() {
+ $('img').on('load', function() {
+      $('img').addClass('tasty');
+   });
+  }
+//
+function pressIt() {
+   $("input#typing").on('keydown', function(key) {
+     if (key.which == 71) {
+       alert('g was pressed');
+       return;
+     }
    });
  }
 
- 
+
+ // function submitIt(){
+ //    $("form").on("submit", function() {
+ //      if ($( "input:first" ).val() === "correct") {
+ //        alert('Your form is going to be submitted now');
+ //        return;
+ //      }
+ //    });
+ //  }
+ function submitIt(){
+   $( "form" ).submit(function() {
+   alert('Your form is going to be submitted now.');
+   return
+ });
+ }
+
 $(document).ready(function(){
 
 getIt();
