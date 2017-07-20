3de0f2cae9a664babab69abca788d6c13513af53
Alexander Bunta <aleksandrbunta@gmail.com>
2017-07-20 19:41:37 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,25 @@
 //define functions here
+function getIt() {
+  $('p').on('click', () => alert('Hey!'));
+}
+function frameIt() {
+  $(document).on('load', () => $('img').addClass('tasty'));
+}
+function pressIt() {
+  $('form').on('keydown', (key) => {
+                            if(key.which == 71){
+                              alert('G was pressed');
+                            }})
+}
+
+function submitIt() {
+  $('form').on('submit', () => alert('Your form is going to be submitted now.'));
+}
 
 $(document).ready(function(){
-
+  getIt();
+  frameIt();
+  pressIt();
 // call functions here
 
 });
