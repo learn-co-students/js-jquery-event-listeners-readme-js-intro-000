fe6cc0f383e5b824c67a99e1047c1d6a931ef8fb
Fakhteh Saadatniaki <fakhteh.saadatniaki@tufts.edu>
2017-07-27 02:40:30 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function(){
+      alert('Hey!');//action you want taken
+  });
+}
+function frameIt() {
+  $('img').on("load", function(){
+      $("img").addClass("tasty");
+  });
+}
+
+function pressIt() {
+  $('form').on("keydown", function(key){
+    if(key.which == 71){
+        alert('g was pressed');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on("submit", function(){
+        alert("Your form is going to be submitted now.");
+  });
+}
 
 $(document).ready(function(){
-
+getIt()
+frameIt()
+pressIt()
 // call functions here
-
+submitIt()
 });
