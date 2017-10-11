e8c56aedcbb8271ecb2a714667197e9373a93670
Oliver Escoto <oliver.jurgen90@gmail.com>
2017-10-11 09:02:01 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,40 @@
 //define functions here
 
+function getIt () {
+  $("p").on("click", function () {
+    alert('hey');
+  })
+}
+
+function frameIt () {
+  $("img").load("load", function() {
+    $("img").attr("class", "tasty")
+  })
+}
+
+function pressIt () {
+  $("input").on("keydown", function (e) {
+    if( e.which === 71 ) {
+        alert('correct value')
+    }
+  })
+}
+
+function submitIt () {
+  $("form").on("submit", function () {
+    alert('Your form is going to be submitted now.')
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
+
+getIt();
+frameIt();
+pressIt();
+submitIt();
+
+
 
 });
