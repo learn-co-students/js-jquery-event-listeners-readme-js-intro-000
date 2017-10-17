e4fa98ee9cfffcb56d3079dbdfc0d0a930daebac
Nikita Neganov <nikitaneganov@icloud.com>
2017-10-17 13:47:11 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,26 @@
 //define functions here
-
+function getIt(){
+  $('p').on('click', (e) =>{
+    alert('Hey!')
+  })
+}
+function frameIt(){
+  $('img').on('load', (e)=>{
+    $('img').addClass("tasty");
+  })
+}
+function pressIt(){
+  $('input').on('keydown', (e)=>{
+    if (parseInt(e.which || e.detail) === 71){
+      alert('You have successfully found a G-spot!')
+    }
+  })
+}
+function submitIt(){
+  $('form').on('submit', (e)=>{
+    alert('Your form is going to be submitted now.');
+  })
+}
 $(document).ready(function(){
 
 // call functions here
