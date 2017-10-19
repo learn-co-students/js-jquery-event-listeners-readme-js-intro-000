d92074d601cde6f344142a119d851622139178e7
Joshua Palacios <palacios.joshua@gmail.com>
2017-10-19 22:41:22 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,31 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').addClass("tasty")
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function () {
+   alert("Your form is going to be submitted now.")
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if (key.which === 71) {
+      alert("You pressed 'G'")
+    } 
+  })
+}
 
 $(document).ready(function(){
 
-// call functions here
 
 });
