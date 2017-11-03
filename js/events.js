6e2957321472a3542ae47c7a5c574db083c5e498
Derrik  Hanson <dh75021@gmail.com>
2017-11-03 09:53:45 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,32 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!")
+  });
+}
+
+
+function frameIt() {
+  $('img').on("load", function() {
+    // $('img').css("border", "red 2px");
+    $('img').addClass( "tasty" );
+  });
+}
+
+function pressIt() {
+  $('#typing').on("keydown", function(key) {
+    if (key.which == 71) {
+      alert('you have pressed G');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on("submit", function() {
+    alert('Your form is going to be submitted now.');
+  }
+)};
