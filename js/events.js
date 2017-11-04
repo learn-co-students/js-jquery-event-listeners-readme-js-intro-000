00c151c62cd3e8f635d92f59d556272e4403224a
Kévin R <kevin.rabesaotra@gmail.com>
2017-11-04 14:43:53 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
+function getIt(){
+
+$('p').on('click', function(){
+  alert('Hey!');
+  })
+
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $(this).addClass('tasty');
+    $('.tasty').css('color', 'red')
+  })
+}
+
+function pressIt(){
+  $('form').on('keydown', function(e){
+    var presse = parseInt(e.which);
+    if(presse === 71){
+      alert('G');
+    }
+  })
+}
+
+function submitIt(){
+$('form').on('submit', function(){
+  alert("Your form is going to be submitted now.")
+  })
+}
 
 $(document).ready(function(){
-
 // call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
