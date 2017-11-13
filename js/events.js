125c68446af6a3c4bf458d3d2536f477e6f4251e
Julio Martelosso <martelossomartin1506.student@ifosupwavre.be>
2017-11-13 15:26:49 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,24 @@
 //define functions here
+function getIt() {
+  $('p').on('click',function(){
+    alert('Hey!')
+  })
+}
+
+function frameIt() {
+
+}
+
+function pressIt() {
+
+}
+
+function submitIt() {
+
+}
 
 $(document).ready(function(){
-
 // call functions here
+getIt();
 
 });
