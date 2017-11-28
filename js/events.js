49aacdf9a11d0ad2d1172a806c8bbd2df60ebd79
Kathy Hudson <khitx@icloud.com>
2017-11-28 23:24:20 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -6,9 +6,8 @@ function getIt() {
 }
 
 function frameIt() {
-  $('img').addClass( "tasty", "border", "3px solid red")
-  $('form').on('load', function(){
-    $('img').addClass( "tasty", "border", "3px solid red")
+  $('img').on('load', function(){
+    $('img').addClass( "tasty")
   });
 }
 
