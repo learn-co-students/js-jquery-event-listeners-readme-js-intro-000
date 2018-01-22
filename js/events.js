343ab1428e3e418d5fadee7b617e73e36d4b3f2d
Lou Angeles <lou.c.angeles@gmail.com>
2018-01-22 05:48:38 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,34 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function() {
+    // action we want taken
+    alert('hey!')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    // load event handler
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(e) {
+    // Keypress 'G' is ascii 71
+    if (e.which === 71) {
+      alert("You pressed 'g'!")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert('Your form is going to be submitted now.')
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
