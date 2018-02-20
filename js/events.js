cade8586409bf6c88a9e2d9f1110a1e1bb160600
Juan David Bermudez Celedon <juanbermucele@hotmail.com>
2018-02-20 19:46:09 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt() {
+  $("p").on("click", () => {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $("img").on("load", function() {
+    $(this).addClass("tasty");
+  });
+}
+
+function pressIt() {
+  $("#typing").on("keydown", (e) => {
+    if (e.detail === 71 || e.location === 71 || e.which === 71) {
+      alert("You pressed the G key");
+    }
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", (e) => {
+    alert("Your form is going to be submitted now.");
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+    frameIt();
+    getIt();
+    pressIt();
+    submitIt();
 });
