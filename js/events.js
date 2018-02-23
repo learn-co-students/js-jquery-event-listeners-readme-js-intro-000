69451dea37f96afc889ea57139571a208d5179ee
Alex Segers <a.segers.dev@gmail.com>
2018-02-23 03:10:11 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    // 'G'.charCodeAt(0) is 71
+    if (key.which == 'G'.charCodeAt(0)) alert(key.which);
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+  })
+}
 
 $(document).ready(function(){
 
-// call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
