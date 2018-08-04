fb26db69bdbd9f2f25575702041c4f748668c5c6
Jonathan Hosein <jonathan.hosein@live.com>
2018-08-04 21:31:51 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
-//define functions here
+//Define functions
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!');
+    return;
+  })
+}
 
+function frameIt(){
+  $(window).on('load', function(){
+    $('img').attr('class','tasty');
+    return;
+  })
+}
+
+function pressIt(){
+  $('form').on('keydown', (e) => {
+    if(e.which === 71){
+      alert("G was pressed!");
+      return;
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', ()=>{
+    alert('Your form is being submitted now.');
+    return;
+  })
+}
+
+//call functions
 $(document).ready(function(){
-
-// call functions here
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 
 });
