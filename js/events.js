7dce9026fa5c5ee70d11ca94751d67940ec09f40
Alena <i.am.am.629@gmail.com>
2018-08-06 23:51:47 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,30 @@
 
 $(document).ready(function(){
 
-// call functions here
+  function getIt() {
+    $('p').on('click', function(){
+      alert('Hey!')
+    });
+  }
 
+  function frameIt() {
+    $(document).on('load', function(){
+      img.classList.add("tasty");
+    });
+  }
+
+  function pressIt() {
+    $(document).on('keydown', function(){
+      if(key.which == 70){
+        alert('g was pressed');
+      }
+    });
+  }
+
+  function submitIt() {
+    $("form").on("submit", function() {
+      alert('your form is going to be submitted now');
+      return;
+    });
+  }
 });
