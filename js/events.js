5ea3a05caca65c96e84c0542c2d00d4386aa28ac
Alena <i.am.am.629@gmail.com>
2018-08-07 00:08:57 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,31 +1,36 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function(){
+    alert('Hey!')
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    'img'.addClass("tasty");
+  });
+}
+
+function pressIt() {
+  $(document).on('keydown', function(){
+    if(key.which == 70){
+      alert('g was pressed');
+    }
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert('your form is going to be submitted now');
+    return;
+  });
+}
+
+
 $(document).ready(function(){
-
-  function getIt() {
-    $('p').on('click', function(){
-      alert('Hey!')
-    });
-  }
-
-  function frameIt() {
-    $(document).on('load', function(){
-      img.classList.add("tasty");
-    });
-  }
-
-  function pressIt() {
-    $(document).on('keydown', function(){
-      if(key.which == 70){
-        alert('g was pressed');
-      }
-    });
-  }
-
-  function submitIt() {
-    $("form").on("submit", function() {
-      alert('your form is going to be submitted now');
-      return;
-    });
-  }
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 });
