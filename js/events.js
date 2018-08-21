51b8e8fc9a4fafd42385bd35bafcfead0e646882
Bhakti Sawarkar <bhakti123.java@gmail.com>
2018-08-21 20:34:44 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
-//define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert ('Hey!')
+  });
+}
+
+function frameIt(){
+  $('img').on('load', function{
+    $('img').addClass("tasty");
+  });
+}
+
+function pressIt(){
+  $('input').on('keydown', function(){
+    if(key.which == 70){
+      alert("G was pressed");
+    }
+  });
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    alert('your form is submitted now');
+  });
+}
+});
+
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+frameIt();
+pressIt();
+submitIt();
 });
