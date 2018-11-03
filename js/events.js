7584327952fe47800149d019e1790ecb8a6af497
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2018-11-03 04:40:01 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,23 @@
-//define functions here
+function getIt() {
+  $('p').on('click', () => alert("Hey!"));
+}
+
+function frameIt() {
+  $('img').on('load', () => $('img').addClass('tasty'));
+}
+
+function pressIt() {
+  $('form input').on('keydown', (key) => (key.which == 71) ? alert("G key was pressed!") : 0);
+}
+
+function submitIt() {
+  $('form').on('submit', () => alert("Your form is going to be submitted now."));
+}
+
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
