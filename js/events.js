5b0add14a484dd37a8e2f2091e9f45392a5e9a8e
Andrei Korchagin <andrei.korchagin@gmail.com>
2019-02-15 15:14:31 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
 //define functions here
+function getIt(){
+  $('p').click(function(){
+    alert("Hey!");
+  });
+}
+function frameIt(){
+  $('img').load(function(){
+    $(this).addClass('tasty');
+  });
+}
+
+function pressIt(){
+  $('form > input').keydown(function(e){
+    if(e.which == 71){
+      alert("You have pressed G");
+    }
+  });
+}
+function submitIt(){
+  $('form').submit(function(){
+    alert("Your form is going to be submitted now.");
+  });
+}
 
 $(document).ready(function(){
-
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
