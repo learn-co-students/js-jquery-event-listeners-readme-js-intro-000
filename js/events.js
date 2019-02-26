ed7c0169f18f57158cf4a6cf9803d89b2eaf691e
lanasgoddard <lanasgoddard@gmail.com>
2019-02-26 09:48:36 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,6 +1,36 @@
-//define functions here
+function getIt(){
+  $('p').click(function(){
+    alert("Hey!")
+});
+
+}
+
+
+function frameIt(){
+  $("img").on("load",(e)=>{
+    var img1=document.querySelector('img');
+     img1.className="tasty"
+  })
+}
+
+function pressIt(){
+  $('form > input').keydown(function(e){
+    if(e.which == 71){
+      alert("You have pressed G");
+    }
+  });
+}
+
+function submitIt(){
+  $('form').submit(function(){
+    alert("Your form is going to be submitted now.");
+  });
+}
+
+
 
 $(document).ready(function(){
+
 
 // call functions here
 
