a1e9651c62b1e4d791756562147440d9c24ec30f
Joe Tustin <j.tustin@gmail.com>
2019-02-28 00:38:20 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -4,14 +4,40 @@ function getIt() {
     alert('Hey!');
   })}
 
+function frameIt() {
+  $("img").load(function(){
+    $("img").addClass("tasty");
+    });
+  $("tasty").load(function(){
+    $(this).css("border","solid 15px red");
+    })
+}
 
+function submitIt() {
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.');
+  })
+}
 
+function pressIt() {
+  $("#typing").on("keydown", function(e) {
+    if (e.which === 71) {
+      alert("You have pressed the G key.");
+    }
+  })
+ }
 
 
 
 $(document).ready(function(){
 
 // call functions here
-getIt()
+getIt();
+
+submitIt();
+
+frameIt();
+
+pressIt();
 
 });
