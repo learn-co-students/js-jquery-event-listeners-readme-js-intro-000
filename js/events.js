9917905a91460fac6cab18259d4807eded5ec89f
Niki Agrawal <nikita24agrawal@gmail.com>
2019-03-04 04:30:55 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,9 @@
 
 $(document).ready(function(){
 getIt();
+frameIt();
+pressIt();
+submitIt();
 // call functions here
 
 });
@@ -11,3 +14,22 @@ $('p').on("click", function(){
 alert('"Hey!"')
 });
 }
+
+function frameIt(){
+  $('img').on('load', function(){
+    $("img").addClass("tasty"); });
+  }
+
+function pressIt(){
+  $(document).on('keydown', function(key){
+    if(key.which == 71){
+     alert('G was pressed');
+   }
+ });
+ }
+
+ function submitIt(){
+   $("form").on("submit", function(){
+     alert('Your form is going to be submitted now.');
+   });
+ }
