197adc0cfab9b4e878f7dfefa1864688c30c56bf
James Marvin <jmarvin74@gmail.com>
2019-05-18 00:14:32 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,22 @@
-//define functions here
-
+function getIt() {
+  $('p').on('click', function () {alert('Hey!');});
+}
+function frameIt() {
+  $('img').on('load', function () {$('img').addClass('tasty')});
+}
+function submitIt() {
+  $('form').on('submit', function() {alert('Your form is going to be submitted now.')});
+}
+function pressIt() {
+  $('input#typing').on('keydown', function(event) {
+    if(event.which===71) {
+      alert('You pressed G!');
+    }
+  });
+}
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  submitIt();
+  pressIt();
 });
