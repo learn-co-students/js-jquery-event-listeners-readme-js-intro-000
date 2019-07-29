886e41e7d94d860d41f72d6761b5a7b41bcb2fe7
Christos Giakoumis <cm.giakoumis@gmail.com>
2019-07-29 20:57:49 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,43 @@
 //define functions here
+function getIt()
+  {
+    $("p").on("click", function()
+      {
+        window.alert("Hey!");
+      });
+  }
 
-$(document).ready(function(){
+function frameIt()
+  {
+    $("img").on("load", function()
+      {
+      $("img").addClass("tasty");
+      });
+  }
+
+function pressIt()
+  {
+    $("form").on("keydown", function(G)
+      {
+        if (G.which===71)
+          {
+            window.alert('G was pressed');
+          }
+      });
+  }
+
+function submitIt()
+    {
+      $("form").on("submit", function()
+        {
+          window.alert("Your form is going to be submitted now.");
+        });
+    }
+
 
 // call functions here
 
-});
+getIt();
+frameIt();
+pressIt();
+submitIt();
