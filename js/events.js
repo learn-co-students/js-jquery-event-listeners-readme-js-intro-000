6d451ac78581160e86317dc1beae6e6a8dcfcea8
Nobuhiko Itakura <n.itakura22@gmail.com>
2019-08-06 10:17:24 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,37 @@
 $(document).ready(function(){
 
 // call functions here
+  getIt();
 
+  frameIt();
+
+  pressIt();
+
+  submitIt();
 });
+
+function getIt() {
+  $("p").on("click", () => {
+    alert("Hey!")
+  });
+}
+
+function frameIt() {
+  $("img").on("load", () => {
+    $("img").addClass("tasty")
+  });
+}
+
+function pressIt() {
+  $("#typing").on("keydown", (key) => {
+    if (key.which === 71) {
+      alert("G was pressed.")
+    }
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", () => {
+    alert("Your form is going to be submitted now.")
+  });
+}
