2d92712286d773f407de601c3476fd7d56d88663
al <albliu@email.com>
2019-12-10 02:39:41 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,34 @@
-//define functions here
+function getIt(){
+  $("p").on("click", function() {
+    alert('Hey!');
+  })
+}//end getIT
+
+function frameIt(){
+  $("img").on("load", function() {
+
+    $('img').addClass("tasty")
+//  $('img').append('<img class="tasty" src="minion-tongue.jpg" />')
+
+  })
+
+}//end frameIT
+
+function pressIt(){
+  $("#typing").on("keydown", function(key) {
+    if (key.which == 71)
+    alert('You pressed the G key');
+  })
+
+}//end pressIT
+
+function submitIt(){
+  $("form").on("submit", function(key) {
+
+    alert('Your form is going to be submitted now.');
+  })
+
+}//end submitit
 
 $(document).ready(function(){
 
