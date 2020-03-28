a9112989a50476a17f5085b55a2eadbcc15cc1fc
funny-teacher-0837 <detinsley1s@gmail.com>
2020-03-28 05:23:23 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,23 @@
 //define functions here
+function getIt() {
+  $('p').on('click', () => alert('Hey!'))
+}
+
+function frameIt() {
+  $('img').addClass('tasty')
+}
+
+function pressIt() {
+  $('form').on('keydown', key => {
+    if(key.which === 71) {
+      alert('You pressed the G key')
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', () => alert('Your form is going to be submitted now.'))
+}
 
 $(document).ready(function(){
 
