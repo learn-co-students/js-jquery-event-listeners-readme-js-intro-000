42499815f5b78221464b1c8775d40e777cd4fc8f
Simon Kilroy <simonkilroy1@gmail.com>
2020-04-08 09:12:08 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,6 +1,38 @@
-//define functions here
+function getIt() {
+$('p').on("click", function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('img').on("load", function() {
+    $('img').addClass("tasty")
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(e) {
+    if (e.which == 71)
+    alert("You've pressed 'g'")
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
 
 $(document).ready(function(){
+
+window.getIt()
+
+window.frameIt()
+
+window.pressIt()
+
+window.submitIt()
+
 
 // call functions here
 
