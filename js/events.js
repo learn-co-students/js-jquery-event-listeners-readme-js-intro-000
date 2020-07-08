1ea44eac21df7bd6d03ecf0adf22a3cadbdfaf1b
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-07-08 14:59:28 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
-//define functions here
+function getIt(){
+  $('p').on('click', e => {
+    alert('Hey!')
+  })
+}
+
+function frameIt(){
+  $('img').on('load', e => {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt(){
+  $('form').on('keydown', e => {
+    key = e.which
+    if (key === 71){
+      alert(`${e.key} has been pressed`)
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', e => {
+    alert("Your form is going to be submitted now.")
+  })
+}
 
 $(document).ready(function(){
 
-// call functions here
+getIt()
+frameIt()
+pressIt()
+submitIt()
 
 });
