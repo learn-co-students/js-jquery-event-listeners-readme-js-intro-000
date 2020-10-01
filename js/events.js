ca1f5e73ec6118d390a9a5a77b2ba4bc4c8bc853
Alexander Petrov <acepetrov96@gmail.com>
2020-10-01 22:15:25 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,29 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('img').on("load", function() {
+    $('img').addClass("tasty")
+  })
+}
+
+function pressIt() {
+  $('form').on("keydown", function(key) {
+    if(key.which == 71){
+      alert("Your form is going to be submitted now.")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  })
+}
 
 $(document).ready(function(){
 
