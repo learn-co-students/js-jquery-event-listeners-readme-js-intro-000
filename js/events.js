d54dfd9b8237a7bff3ab58113139330dbad4e4a4
Cole Cezeaux <colecezeaux@gmail.com>
2020-10-29 17:32:59 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,29 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function(){
+    alert("Hey!")
+});
+}
+
+function frameIt() {
+  $('img').addClass("tasty")
+  $('img').on('load', function(){
+});
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if(key.which == 71) {
+      alert('g was pressed')
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
 
 $(document).ready(function(){
 
