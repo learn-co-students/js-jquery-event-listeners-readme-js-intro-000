98102feb231612057ff4bd7fb97006c4c14b27ef
JINOOK JUNG <jinook929@gmail.com>
2020-11-08 03:01:18 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,31 @@
 //define functions here
+function getIt() {
+  $('p').on('click', e => { alert('Hey!') });
+}
+
+function frameIt() {
+  $('img').addClass('tasty');
+}
+
+function pressIt() {
+  $('input').on('keydown', e => {
+    if (e.which === 71) {
+      // e.preventDefault();
+      alert('You have pressed the G key.')
+    }
+  });
+}
+
+function submitIt() {
+  $("form").submit(e => { alert('Your form is going to be submitted now.'); });
+}
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt()
 
 });
