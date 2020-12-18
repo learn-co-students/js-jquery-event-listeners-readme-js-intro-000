fb83df0063b0018a2951b8e06fb8d91d2fdad324
A.J. Romaniello <ajiellodev@gmail.com>
2020-12-18 19:51:25 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,28 @@
 //define functions here
+function getIt() {
+  $('p').on('click', alert("Hey!"));
+}
 
-$(document).ready(function(){
+function frameIt() {
+  $('img').on("load", 'img', $('img').addClass('tasty'));
+}
 
-// call functions here
+function pressIt() {
+  $('#typing').on("keydown", function(key) {
+    if (key.which === 71) {
+      alert('G was pressed')
+    }
+  })
+}
 
+function submitIt() {
+  $('input[type="submit"]').on('submit', alert("Your form is going to be submitted now."))
+}
+
+
+$(document).ready(function() {
+  getIt(),
+  frameIt(),
+  pressIt(),
+  submitIt()
 });
