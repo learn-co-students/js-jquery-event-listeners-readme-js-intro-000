53f42245bafb7ce04c36c4343cc6d7e7afee40e2
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2021-02-24 10:53:43 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -6,13 +6,30 @@ function getIt() {
 }
 
 function frameIt() { //bind the load event that adds the class tasty to the image to add a red frame to the image
-  $().on("load", function(){
-     
+  $('img').on("load", function(){
+     $('img').addClass('tasty');
   });
 }
 
-$(document).ready(function(){
+function pressIt() {  //bind a keydown event to the input field of the form that alerts a user when they have pressed the G key
+  $('input').on("keydown", function(key){
+    if (key.which == 71) {
+    	alert("You pressed G!");
+    }
+  });
+}
+
+function submitIt() { //function should bind a submit event to the form that alerts "Your form is going to be submitted now."
+  $('form').on("submit", function(key){
+  	alert("Your form is going to be submitted now.");
+  });
+}
+
+$(document).ready(function(){  //
 
 // call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
