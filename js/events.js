694f43772cc3a78299582027e0304f033a744f15
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2021-02-24 10:21:18 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,15 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function(){
+      alert("Hey!");
+  });
+}
+
+function frameIt() { //bind the load event that adds the class tasty to the image to add a red frame to the image
+  $().on("load", function(){
+     
+  });
+}
 
 $(document).ready(function(){
 
