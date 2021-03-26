2f6e75c9c88d9ac1d02fac7e1f14eb44ec670b47
ji sun lee <ellijs@hotmail.com>
2021-03-26 06:13:08 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,30 @@
-//define functions here
+function getIt() {
+    alert("Hey");
+}
+$("p").on("Click", getIt); 
+
+
+function frameIt() {
+  $('.tasty').css('background', 'red');
+}
+$(".tasty").on("load", frameIt);
+
+
+function pressIt() {
+  $("input").keydown(function() {
+    if(key.which == 71) {
+      alert("You pressed G key");
+     }
+  });
+}
+
+function submitIt() {
+  alert("Your form is going to be submitted now.")
+}
+$("form").on("submit", submitIt);
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  pressIt();
+  submitIt();
 });
