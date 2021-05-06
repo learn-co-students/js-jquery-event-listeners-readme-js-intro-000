846914c82eed39d5d0a0bc2647270bf1a2e4ca1b
Jake Peterson <jpeterson0514@gmail.com>
2021-05-06 20:58:59 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
+function getIt(){
+	$('p').click(function(){
+		alert("Hey!");
+	});
+}
 
+function frameIt(){
+	$('img').load(function(){
+		$(this).addClass('tasty');
+	});
+}
+
+function pressIt(){
+	$('input').on('keydown', function(e){
+		if(e.which == 71 || e.which == 103){
+			alert("you pressed G");
+		}
+	});
+}
+
+function submitIt(){
+	$("form").on('submit', function(){
+		alert("Your form is going to be submitted now.");
+	});
+}
 $(document).ready(function(){
 
 // call functions here
-
+pressIt();
+submitIt();
+frameIt();
+getIt();
 });
