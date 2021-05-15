b25a612ad5ad032e3bd4f987faf13bc028d353c6
DANIEL PAUL SEMINARA <danseminara@yahoo.com>
2021-05-15 16:06:48 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -4,4 +4,41 @@ $(document).ready(function(){
 
 // call functions here
 
+getIt()
+submitIt()
+pressIt()
+frameIt()
+
 });
+
+
+function getIt() {
+  $('p').on("click", function() {alert("Hey!") });
+}
+
+function submitIt() {
+  
+  $("form").on("submit", function() { 
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+
+
+
+function pressIt() {  
+  
+  $(document).on('keydown', function(key) { 
+    console.log(key.which)
+    if (key.which == 71){   alert('You have pressed the G key.');}
+    
+  });
+}
+
+function frameIt() {
+  
+  $('img').on('load', function(){ 
+    
+    $("img").addClass("tasty")
+  });
+}
