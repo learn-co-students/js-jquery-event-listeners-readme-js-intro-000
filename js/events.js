93a79a394e32d76ff35822682c9e40c809990c5a
Daniel Calvin <arockne2003@yahoo.com>
2021-05-22 23:59:55 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', () => alert('Hey!'));
+}
+
+function frameIt() {
+  $('img').on('load', function() { 
+    $(this).addClass('tasty');
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+  });
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if (key.which === 71) {
+      alert('You have pressed g.');
+    }
+  });
+}
+
 $(document).ready(function(){
-
 // call functions here
-
+getIt();
+frameIt();
+submitIt();
 });
